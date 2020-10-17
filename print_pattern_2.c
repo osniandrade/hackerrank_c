dcd070bd103d4a43fc11c861a759a1bd87c62660
osni junior <osniandrade@gmail.com>
2020-10-17 15:28:42 -0300

    print_pattern not quite working yet

--- print_pattern_2.c
+++ print_pattern_2.c
@@ -5,11 +5,41 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() 
+void printc(int n, int cx, int cy)
 {
+    int max = (n * 2) - 1;
 
-    int n = 4;
+    if (cy == 0 || cy == (max - 1))
+        printf("%d", n);
+    else
+        if (cx == 0 || cx == (max - 1))
+            printf("%d", n);
+        else
+            printf("%d", abs(n - cx));
+    if (cy == (max - 1))
+        printf("\n");
+    else
+        printf("_");
+}
+
+int main()
+{
+    int n = 7;
     // scanf("%d", &n);
+    int cx = 0;
+    int cy = 0;
+    int max = (n * 2) - 1;
+
+    while (cx < max)
+    {
+        while (cy < max)
+        {
+            printc(n, cx, cy);
+            cy++;
+        }
+        cy = 0;
+        cx++;
+    }
   	
-    return 0;
+    return (0);
 }
